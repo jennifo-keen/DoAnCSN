c72d077c10a36e24694b23aec5614e613aef2122
Kien <Email ca bn>
2025-03-10 00:05:32 +0700

    azz

--- frontend/src/page/user/theme/footer/index.js
+++ frontend/src/page/user/theme/footer/index.js
@@ -6,7 +6,7 @@ const Footer = () => {
         <footer>
         <div class="footer_1">
             <div>
-                <img  class="footer_logo" src="Logo.png" alt="logo"/>
+                <img  class="footer_logo" src="/Logo.png" alt="logo"/>
             </div>
             <div class="footer_conn">
                 <h2>Sản phẩm</h2>
